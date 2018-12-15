d7cbf601071eff2e626da279828aa79fc6522388
Jerry <jerryartist@gmail.com>
2018-12-14 16:34:24 -0800

    added art backgrounds and objects for level map

--- tutorial_step5/js/resources.js
+++ tutorial_step5/js/resources.js
@@ -4,6 +4,10 @@ game.resources = [
 	 */
 	// our level tileset
 	{name: "area01_level_tiles",  type:"image",	src: "data/img/map/area01_level_tiles.png"},
+	{name: "village-tiles5",  type:"image",	src: "data/img/map/village-tiles5.png"},
+  {name: "village-pagoda3",  type:"image",	src: "data/img/map/village-pagoda3.png"},
+
+
 	// the main player spritesheet
 	//{name: "gripe_run_right",     type:"image",	src: "data/img/sprite/gripe_run_right.png"},
 	{name: "walk-right-yellow2",     type:"image",	src: "data/img/sprite/walk-right-yellow2.png"},
@@ -14,6 +18,11 @@ game.resources = [
 	// the parallax background
 	{name: "area01_bkg0",         type:"image",	src: "data/img/area01_bkg0.png"},
 	{name: "area01_bkg1",         type:"image",	src: "data/img/area01_bkg1.png"},
+	// {name: "mountains-background",         type:"image",	src: "data/img/mountains-background.png"},
+	// {name: "mountain-sky",         type:"image",	src: "data/img/mountain-sky.png"},
+	// {name: "mountains-background3",         type:"image",	src: "data/img/mountains-background3.png"},
+	{name: "mountains-background4",         type:"image",	src: "data/img/mountains-background4.png"},
+
 	// the spinning coin spritesheet
 	{name: "spinning_coin_gold",  type:"image",	src: "data/img/sprite/spinning_coin_gold.png"},
 	// our enemty entity
@@ -23,7 +32,8 @@ game.resources = [
 	/*
 	 * Maps.
  	 */
-	{name: "area01",              type: "tmx",	src: "data/map/area01.tmx"}
+	{name: "area01",              type: "tmx",	src: "data/map/area01.tmx"},
+	{name: "aikidomap1",              type: "tmx",	src: "data/map/aikidomap1.tmx"},
 
 	/*
 	 * Background music.
